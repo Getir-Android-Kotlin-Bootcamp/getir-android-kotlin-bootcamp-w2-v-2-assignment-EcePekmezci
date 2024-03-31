b991294f135b330b03461289db91e1be8a09c3df
EcePekmezci <92378146+EcePekmezci@users.noreply.github.com>
2024-03-31 21:15:23 +0300

    MapAddedTo MainActivity.kt

--- app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
@@ -3,63 +3,25 @@ package com.getir.patika.foodcouriers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager2: ViewPager2
-    private lateinit var pagerAdapter: PagerAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mGoogleMap: GoogleMap? = null
+    override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account)
-        tabLayout = findViewById(R.id.tab_account)
-        viewPager2 = findViewById(R.id.viewpager_account)
-        pagerAdapter = PagerAdapter(supportFragmentManager,lifecycle).apply {
-            addFragment(CreateAccountFragment())
-            addFragment(LoginAccountFragment())
-        }
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        setLocationButton = findViewById(R.id.setLocationButton)
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        setContentView(R.layout.activity_main)
+
+        val mapFragment=supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        setLocationButton.setOnClickListener {
-        }
-        viewPager2.adapter = pagerAdapter
-
-        TabLayoutMediator(tabLayout,viewPager2){ tab, position ->
-             when(position) {
-                 0 -> {
-                     tab.text = "Create Account"
-                 }
-                 1 -> {
-                     tab.text = "Login Account"
-                 }
-             }
-
-        }.attach()
-
-        override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-            }
-        }
+    }
+    override fun onMapReady(googleMap: GoogleMap){
+        mGoogleMap=googleMap
     }
 }
+
